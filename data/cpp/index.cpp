b814d117c8f0652ad8d166ecc42ac9abcd290039
abril <abrilgutierrez66658@gmail.com>
2021-07-30 17:10:10 -0500

    first commit

---
+++ data/cpp/index.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+        int number;
+
+        std::cout << "Enter a number";
+        std::cin >> number;
+        std::cout << "Perfect!"
+
+        system("pause");
+        return 0;
+}
